0cf6053d90ac87779b4ef6cf123785791a5ab1f5
pawnlord <scruffduff321@gmail.com>
2024-04-22 11:33:13 -0400

    Cleanup input.h

--- input.h
+++ input.h
@@ -40,62 +40,14 @@ struct InputManager {
     using Func = std::pair<KeyboardKey, std::function<void(void)>>;
     std::unordered_map<std::string, Func> functionalities;
     std::unordered_map<KeyboardKey, InputInformation> inputs;
-    MapResult add_functionality(std::string name, std::function<void(void)> functionality) {
-        if (functionalities.find(name) != functionalities.end()) {
-            return MapResult::FunctionalityExists;
-        }
-        functionalities.emplace(name, Func(KEY_NULL, functionality));
-        return MapResult::Ok;
-    }
+    MapResult add_functionality(std::string name, std::function<void(void)> functionality);
+    MapResult map_button(KeyboardKey key, KeyState activation_state, std::string func_name);
 
-    MapResult map_button(KeyboardKey key, KeyState activation_state, std::string func_name) {
-        auto func_pair = functionalities.find(func_name);
-        if (func_pair == functionalities.end()) {
-            return MapResult::FunctionalityDoesntExist;
-        }
+    void set_button(KeyboardKey key, KeyState activation_state, std::string func_name);
 
-        auto& func = func_pair->second;
-        if (func.first != KEY_NULL) {
-            return MapResult::FuncAlreadyMapped;
-        }
-        if(inputs.find(key) != inputs.end()) {
-            return MapResult::KeyAlreadyMapped;
-        }
+    void unmap_button(KeyboardKey key);
 
-        func.first = key;
-        inputs.emplace(key, InputInformation {key, activation_state, func.second, func_name});
-
-        return MapResult::Ok;
-    }
-
-    void set_button(KeyboardKey key, KeyState activation_state, std::string func_name) {
-        MapResult res = map_button(key, activation_state, func_name);
-        if (res == MapResult::FunctionalityDoesntExist) {
-            throw new std::exception(("Functionality " + func_name + "  does not exist.").c_str());
-        }
-        
-        if (res == MapResult::FuncAlreadyMapped) {
-            unmap_function(func_name);
-            res = map_button(key, activation_state, func_name);
-        }
-        
-        if (res == MapResult::KeyAlreadyMapped) {
-            unmap_button(key);
-            map_button(key, activation_state, func_name);
-        }
-    }
-
-    void unmap_button(KeyboardKey key) {
-        InputInformation info = inputs.at(key);
-        inputs.erase(key);
-        functionalities.at(info.function_name).first = KEY_NULL;
-    }
-
-    void unmap_function(std::string func_name) {
-        auto& func_pair = functionalities.at(func_name);
-        inputs.erase(func_pair.first);
-        func_pair.first = KEY_NULL;
-    }
+    void unmap_function(std::string func_name);
 };
 
 }
