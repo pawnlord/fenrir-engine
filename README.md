198bf9ed9ace0665b8d4e2f7a85ba23d3e103264
pawnlord <scruffduff321@gmail.com>
2024-04-22 11:29:31 -0400

    Add input system

--- README.md
+++ README.md
@@ -21,6 +21,18 @@ Entity: Contains the texture and transform, holds components
 Component: A behavior, something that runs every frame
 Tag: A piece of information about the entity
 
+## Input
+The necessary components of an input system are as follows:
+- A simple mapping from input action to functionality
+- A way to keep state between actions
+- Serialization and deserialization
+I would prefer for the method of keeping state to **not** be global variables.
+Heres the current idea:
+```
+InputInformation: to be inherited from. Contains the callback and metadata.
+By making this not  
+```
+
 ## TODO
 1. Better debug information
 2. Testing for Rectangle collisions
---
+++ tests/input.cpp
@@ -0,0 +1,40 @@
+#include <input.h>
+
+#include <raylib.h>
+#include <gtest/gtest.h>
+
+
+TEST(InputTest, BasicAssertions) {
+    vl::InputManager im;
+    int x = 0;
+    int y = 0;
+
+    im.add_functionality("Up", [&](){
+        y += 1;
+    });
+    
+    im.add_functionality("Left", [&](){
+        x -= 1;
+    });
+
+    im.set_button(KEY_W, vl::KeyState::Up, "Up");
+    ASSERT_EQ(im.functionalities.at("Up").first, KEY_W);
+    ASSERT_EQ(im.inputs.at(KEY_W).function_name, "Up");
+    
+    im.inputs.at(KEY_W).callback();
+    ASSERT_EQ(y, 1);
+
+    im.set_button(KEY_W, vl::KeyState::Up, "Left");
+    ASSERT_EQ(im.functionalities.at("Up").first, KEY_NULL);
+    ASSERT_EQ(im.functionalities.at("Left").first, KEY_W);
+    ASSERT_EQ(im.inputs.at(KEY_W).function_name, "Left");
+
+    im.inputs.at(KEY_W).callback();
+    ASSERT_EQ(x, -1);
+
+    im.set_button(KEY_A, vl::KeyState::Up, "Left");
+    ASSERT_EQ(im.inputs.find(KEY_W), im.inputs.end());
+    ASSERT_EQ(im.functionalities.at("Left").first, KEY_A);
+    ASSERT_EQ(im.inputs.at(KEY_A).function_name, "Left");
+
+}
